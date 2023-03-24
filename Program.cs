c4c7ab1276cffee1c0b8a849c53698329da11168
janrzepisko <rzejan@gmail.com>
2023-03-24 08:15:09 +0100

    origins

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ builder.Services.AddCors(c =>
         .AllowAnyHeader()
         .AllowAnyMethod()
         //.WithOrigins("91.227.2.183:5003", "http://127.0.0.1:5500")
-        //.WithOrigins("91.227.2.183:5003", "http://91.227.2.183:85")
-        .WithOrigins("http://127.0.0.1:5500'", "http://localhost:5500", "127.0.0.1:5003", "*")
+        .WithOrigins("91.227.2.183:5003", "http://91.227.2.183:85","http://127.0.0.1:5500'", "http://localhost:5500", "127.0.0.1:5003", "*")
+        //.WithOrigins()
         .AllowCredentials());
 });
 builder.Services.AddDistributedMemoryCache();
